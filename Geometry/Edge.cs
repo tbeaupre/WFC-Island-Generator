7d83546c45723dc8b168602577836ca2a28c7eb5
Tyler Beaupre <tbeaupre@wpi.edu>
2022-03-25 08:43:26 -0400

    Moves Geometry-related classes to separate files

---
+++ Geometry/Edge.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edge
+{
+    public Vector2 p1;
+    public Vector2 p2;
+
+    public Edge(Vector2 p1, Vector2 p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public static bool operator ==(Edge e1, Edge e2)
+    {
+        return (e1.p1 == e2.p1 && e1.p2 == e2.p2) || (e1.p1 == e2.p2 && e1.p2 == e2.p1);
+    }
+
+    public static bool operator !=(Edge e1, Edge e2)
+    {
+        return (e1.p1 != e2.p1 || e1.p2 != e2.p2) && (e1.p1 != e2.p2 || e1.p2 != e2.p1);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Edge e = (Edge)obj;
+        return (this.p1 == e.p1 && this.p2 == e.p2) || (this.p1 == e.p2 && this.p2 == e.p1);
+    }
+
+    public override string ToString()
+    {
+        return "Edge P1: " + p1.ToString() + ", P2: " + p2.ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        return p1.GetHashCode() ^ p2.GetHashCode();
+    }
+}
---
+++ Geometry/Triangle.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Triangle
+{
+    public List<Vector2> vertices;
+    public List<Edge> edges;
+
+    public Triangle(List<Vector2> vertices)
+    {
+        this.vertices = vertices;
+        this.edges = new List<Edge>()
+        {
+            new Edge(vertices[0], vertices[1]),
+            new Edge(vertices[1], vertices[2]),
+            new Edge(vertices[2], vertices[0]),
+        };
+    }
+
+    public static bool operator ==(Triangle t1, Triangle t2)
+    {
+        return ListEquals(t1.edges, t2.edges) && ListEquals(t1.vertices, t2.vertices);
+    }
+
+    public static bool operator !=(Triangle t1, Triangle t2)
+    {
+        return !ListEquals(t1.edges, t2.edges) || !ListEquals(t1.vertices, t2.vertices);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+
+        Triangle t = (Triangle)obj;
+        return ListEquals(this.edges, t.edges) && ListEquals(this.vertices, t.vertices);
+    }
+
+    public override int GetHashCode()
+    {
+        return vertices.GetHashCode() ^ edges.GetHashCode();
+    }
+
+    public static bool ListEquals<T>(IList<T> l1, IList<T> l2)
+    {
+        if (l1.Count != l2.Count)
+        {
+            return false;
+        }
+        for (int i = 0; i < l1.Count; ++i)
+        {
+            if (!l1[i].Equals(l2[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public override string ToString()
+    {
+        return "Triangle \nP1: " + vertices[0].ToString() + " P2: " + vertices[1].ToString() + " P3: " + vertices[2].ToString();
+    }
+}
