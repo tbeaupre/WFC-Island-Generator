816db18a6f7b50dca025c840186000735d5323de
Tyler Beaupre <tbeaupre@wpi.edu>
2022-12-01 20:42:48 -0500

    Adds left/right camera controls

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, -20 * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        }
+    }
+}
