b75b322e448af876bc24bbe7d34a1f97a1b4fd0d
Tyler Beaupre <tbeaupre@wpi.edu>
2022-12-16 22:05:03 -0500

    Increases rotation speed

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -9,11 +9,11 @@ public class CameraController : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.RotateAround(Vector3.zero, Vector3.up, -20 * Time.deltaTime);
+            transform.RotateAround(Vector3.zero, Vector3.up, -40 * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+            transform.RotateAround(Vector3.zero, Vector3.up, 40 * Time.deltaTime);
         }
     }
 }
